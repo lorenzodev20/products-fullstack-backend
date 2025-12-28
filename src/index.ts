5c038f71a7ebf45478fb9b404321356c8d76eb2a
Lorenzo Antonio Rojo Garces <lorenzodev2020@gmail.com>
2025-12-28 17:39:59 -0500

    feat: add cors configuration

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import server from './server';
 
 const port = process.env.PORT || 4000;
 
-server.listen(4000, () => {
+server.listen(port, () => {
     console.log(colors.cyan.bold(`REST API en el puerto ${port}`));
 });
--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,10 @@ import express from 'express';
 import router from './router';
 import db from './config/db';
 import colors from 'colors';
-import swaggerUi from 'swagger-ui-express';
+import cors, { CorsOptions } from 'cors';
+import swaggerUi, { serve } from 'swagger-ui-express';
 import swaggerSpec, { swaggerUiOptions } from './config/swagger';
+import morgan from 'morgan';
 
 export async function connectDb() {
     try {
@@ -20,9 +22,27 @@ connectDb();
 
 const server = express();
 
+// CORS
+const whitelist = [`http://localhost:${process.env.PORT}`, process.env.FRONTEND_URL];
+
+const corsOptions: CorsOptions = {
+    // origin: function(origin, callback) {
+    //     console.log(origin)
+    //     if (whitelist.indexOf(origin) !== -1) {
+    //         callback(null, true);
+    //     }else{
+    //         callback(new Error('Error de CORS'));
+    //     }
+    // }
+    origin: whitelist
+};
+
+server.use(cors(corsOptions));
+
 // Leer datos
 server.use(express.json());
 
+server.use(morgan('dev'));
 server.use('/api/products', router);
 
 server.use('/docs',swaggerUi.serve, swaggerUi.setup(swaggerSpec, swaggerUiOptions));
